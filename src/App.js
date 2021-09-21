3768333d53b4c88a353560d417ddc6f98d03d059
Yang <qiaoyanandyang@gmail.com>
2021-09-21 12:00:20 +0800

    Preparing The Project For The Next Steps-Firebase

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import React, { useState, useEffect, useCallback } from "react";
+import React, { useState, useEffect, useCallback } from 'react';
 
-import MoviesList from "./components/MoviesList";
-import "./App.css";
+import MoviesList from './components/MoviesList';
+import AddMovie from './components/AddMovie';
+import './App.css';
 
 function App() {
   const [movies, setMovies] = useState([]);
@@ -11,16 +12,15 @@ function App() {
   const fetchMoviesHandler = useCallback(async () => {
     setIsLoading(true);
     setError(null);
-
     try {
-      const response = await fetch("https://swapi.dev/api/films");
-
+      const response = await fetch('https://movie-app-53da7-default-rtdb.asia-southeast1.firebasedatabase.app/movies.json');
       if (!response.ok) {
-        throw new Error("Something went wrong");
+        throw new Error('Something went wrong!');
       }
 
       const data = await response.json();
-      const transformedMovies = await data.results.map((movieData) => {
+
+      const transformedMovies = data.results.map((movieData) => {
         return {
           id: movieData.episode_id,
           title: movieData.title,
@@ -32,13 +32,16 @@ function App() {
     } catch (error) {
       setError(error.message);
     }
-
     setIsLoading(false);
   }, []);
 
   useEffect(() => {
     fetchMoviesHandler();
   }, [fetchMoviesHandler]);
+
+  function addMovieHandler(movie) {
+    console.log(movie);
+  }
 
   let content = <p>Found no movies.</p>;
 
@@ -56,6 +59,9 @@ function App() {
 
   return (
     <React.Fragment>
+      <section>
+        <AddMovie onAddMovie={addMovieHandler} />
+      </section>
       <section>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
